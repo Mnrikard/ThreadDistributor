6cc2997db173b263063186bdce9d0ff23abb9727
Matthew <rugg@randomguid.net>
2015-08-12 00:14:48 -0400

    comments and handling WorkerThread's complete event

--- ThreadDistributor/Distributor.cs
+++ ThreadDistributor/Distributor.cs
@@ -5,12 +5,27 @@ using System.Linq;
 
 namespace ThreadDistributor
 {
+	/// <summary>
+	/// Obtains work items and distributes them to available threads.
+	/// </summary>
 	public class Distributor
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ThreadDistributor.Distributor"/> class.
+		/// </summary>
+		/// <param name="getMoreWork">Get more work function 
+		/// <para>Takes an integer max number of work items to retrieve and</para>
+		/// <para>returns a list of objects representing the work items.</para>
+		/// </param>
+		/// <param name="workerAction">Action method which takes a single object work item and acts upon it.</param>
+		/// <param name="threadCount">The desired number of worker threads</param>
+		/// <param name="checkInterval">A TimeSpan describing how often to check for work.
+		/// <para>On successfully completing a WorkerAction, GetMoreWork is called again before the timer elapses.</para>
+		/// </param>
 		public Distributor (Func<int, List<object>> getMoreWork, Action<object> workerAction, int threadCount, TimeSpan checkInterval)
 		{
 			GetMoreWork = getMoreWork;
-			_timer = new Timer (DispatchThreads);
+			_timer = new Timer(DispatchThreads, _dispatchResetEvent, Timeout.Infinite, Timeout.Infinite);
 			_timerInterval = checkInterval;
 
 			_availableThreads = new List<WorkerThread>(threadCount);
@@ -18,28 +33,46 @@ namespace ThreadDistributor
 			{
 				_availableThreads[i] = new WorkerThread(workerAction);
 				_availableThreads[i].WorkerException += HandleWorkerException;
+				_availableThreads[i].WorkItemComplete += HandleWorkerItemComplete;
 			}
 		}
 
+		/// <summary>
+		/// Starts the distribution.
+		/// </summary>
 		public void StartDistribution()
 		{
 			_timer.Change (TimeSpan.FromSeconds(0), _timerInterval);
 		}
 
+		/// <summary>
+		/// Stops the distribution.
+		/// </summary>
 		public void StopDistribution()
 		{
 			_timer.Change (Timeout.Infinite, Timeout.Infinite);
 			_stopping = true;
 		}
 
-
+		/// <summary>
+		/// Gets or sets the function which will get more work for the <see cref="WorkerThread"/>s.
+		/// </summary>
+		/// <remarks>
+		/// <para>Takes an integer max number of work items to retrieve and</para>
+		/// <para>returns a list of objects representing the work items.</para>
+		/// </remarks>
 		public Func<int,List<object>> GetMoreWork { get; set; }
 
 		private Timer _timer;
 		private List<WorkerThread> _availableThreads;
 		private TimeSpan _timerInterval;
 		private bool _stopping;
+		private AutoResetEvent _dispatchResetEvent = new AutoResetEvent(false);
 
+		/// <summary>
+		/// Dispatchs the threads, managing the AutoResetEvent.
+		/// </summary>
+		/// <param name="evt">The AutoResetEvent.</param>
 		private void DispatchThreads(object evt)
 		{
 			((AutoResetEvent)evt).WaitOne();
@@ -63,6 +96,9 @@ namespace ThreadDistributor
 			}
 		}
 
+		/// <summary>
+		/// Dispatchs the threads.
+		/// </summary>
 		private void DispatchThreads()
 		{
 			List<WorkerThread> availableWorkers = _availableThreads.Where (t => !t.Busy).ToList ();
@@ -84,8 +120,14 @@ namespace ThreadDistributor
 			}
 		}
 
+		/// <summary>
+		/// Occurs when there are available threads, but no more work.
+		/// </summary>
 		public event EventHandler WorkItemsCleared;
 
+		/// <summary>
+		/// Raises the work items cleared event.
+		/// </summary>
 		private void OnWorkItemsCleared()
 		{
 			if(WorkItemsCleared != null)
@@ -94,8 +136,15 @@ namespace ThreadDistributor
 			}
 		}
 
+		/// <summary>
+		/// Occurs when an the <see cref="Distributor"/> or the <see cref="WorkerThread"/> encounters and error.
+		/// </summary>
 		public event EventHandler<System.UnhandledExceptionEventArgs> ExceptionOccurred;
 
+		/// <summary>
+		/// Raises the exception occurred event.
+		/// </summary>
+		/// <param name="ee">Ee.</param>
 		private void OnExceptionOccurred(Exception ee)
 		{
 			if(ExceptionOccurred != null)
@@ -104,12 +153,27 @@ namespace ThreadDistributor
 			}
 		}
 
+		/// <summary>
+		/// Handles the worker exception.
+		/// </summary>
+		/// <param name="sender">The Sender.</param>
+		/// <param name="e">The exception</param>
 		private void HandleWorkerException(object sender, UnhandledExceptionEventArgs e)
 		{
 			if(ExceptionOccurred != null)
 			{
 				ExceptionOccurred (sender, e);
 			}
+		}
+
+		/// <summary>
+		/// Handles the worker item completing.
+		/// </summary>
+		/// <param name="sender">The Sender.</param>
+		/// <param name="e">Empty</param>
+		private void HandleWorkerItemComplete(object sender, EventArgs e)
+		{
+			DispatchThreads(_dispatchResetEvent);
 		}
 
 	}
--- ThreadDistributor/WorkerThread.cs
+++ ThreadDistributor/WorkerThread.cs
@@ -2,18 +2,36 @@ using System;
 
 namespace ThreadDistributor
 {
+	/// <summary>
+	/// Worker thread.
+	/// </summary>
 	public class WorkerThread
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ThreadDistributor.WorkerThread"/> class.
+		/// </summary>
+		/// <param name="workerAction">Worker action to be performed.</param>
 		public WorkerThread (Action<object> workerAction)
 		{
 			WorkerAction = workerAction;
 		}
 
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="ThreadDistributor.WorkerThread"/> is busy.
+		/// </summary>
+		/// <value><c>true</c> if busy; otherwise, <c>false</c>.</value>
 		public bool Busy { get; set; }
 
+		/// <summary>
+		/// Gets or sets the worker action which will act upon an object WorkItem returned from <see cref="Distributor.GetMoreWork"/>.
+		/// </summary>
 		public Action<object> WorkerAction { get; set; }
 
-		public void CompleteWork(object workItem)
+		/// <summary>
+		/// Completes the work assigned to it.
+		/// </summary>
+		/// <param name="workItem">Work item to be acted upon.</param>
+		internal void CompleteWork(object workItem)
 		{
 			try
 			{
@@ -26,16 +44,39 @@ namespace ThreadDistributor
 			finally
 			{
 				Busy = false;
+				OnWorkItemComplete();
 			}
 		}
 
+		/// <summary>
+		/// Occurs when a WorkerThread throws an exception.
+		/// </summary>
 		internal event EventHandler<UnhandledExceptionEventArgs> WorkerException;
 
+		/// <summary>
+		/// Raises the worker exception event.
+		/// </summary>
+		/// <param name="ee">Ee.</param>
 		private void OnWorkerException(Exception ee)
 		{
 			if(WorkerException != null)
 			{
 				WorkerException(this, new UnhandledExceptionEventArgs (ee, false));
+			}
+		}
+
+		/// <summary>
+		/// Occurs when work item completes.
+		/// </summary>
+		internal event EventHandler WorkItemComplete;
+		/// <summary>
+		/// Raises the work item complete event.
+		/// </summary>
+		private void OnWorkItemComplete()
+		{
+			if(WorkItemComplete != null)
+			{
+				WorkItemComplete(this, EventArgs.Empty);
 			}
 		}
 	}
